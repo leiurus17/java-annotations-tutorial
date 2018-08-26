a6438da2285e66056156158718b032310659b0ab
Marlon Monzon <martianworm17@gmail.com>
2018-08-26 11:11:13 +0800

    Some basic annotations

--- src/com/gowpar/annotations/AnnotationsDemo.java
+++ src/com/gowpar/annotations/AnnotationsDemo.java
@@ -1,5 +1,12 @@
 package com.gowpar.annotations;
 
+import java.util.ArrayList;
+
+@FunctionalInterface
+interface C {
+    void show();
+}
+
 class A {
 
     public void show() {
@@ -9,8 +16,17 @@ class A {
 
 class B extends A {
 
+    @Override
+    @SuppressWarnings("unchecked")
     public void show() {
+
+        ArrayList<Integer> obj = new ArrayList<>();
         System.out.println("Show in Class B.");
+    }
+
+    @Deprecated
+    public void run() {
+        System.out.println("Run in Class B.");
     }
 
 }
@@ -23,5 +39,6 @@ public class AnnotationsDemo {
 
         a.show();
         b.show();
+        b.run();
     }
 }
